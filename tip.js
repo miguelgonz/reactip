98ec1243141d5c1ed6e938cfe4274b0a8da276ef
miguelgonz <Miguel.Gonzalez@bbc.co.uk>
2015-07-27 10:36:09 +0100

    Initial version

---
+++ tip.js
@@ -0,0 +1,105 @@
+var Name = React.createClass({
+  render: function () {
+    return (<div className="name">
+        <div className="name__content">
+            <div className="name__title">{this.props.title}</div>
+            <div className="name__subtitle">{this.props.subtitle}</div>
+        </div>
+      </div>)
+  }
+});
+
+
+var PlayButton = React.createClass({
+  handlePlay: function () {
+    console.log('play');
+     this.props.onPlay();
+  },
+  render: function () {
+    return (
+      <div onClick={this.handlePlay} className="play-button">
+        <span className="icon">►</span>
+      </div>
+    )
+  }
+});
+    
+var Carousel = React.createClass({
+  handleLeft: function () {
+    console.log('go left');
+  },
+  handleRight: function () {
+    console.log('go right');
+  },
+  render: function () {
+    var episodes = this.props.episodes.map(function (ep) {
+      return (
+        <div className="episode">
+          <div className="episode__image"><img src="http://fillmurray.com/200/120"/></div>
+          <div className="episode__title">{ep.title}</div>
+          <div className="episode__subtitle">{ep.subtitle}</div>
+        </div>
+      )
+    });
+    return (
+      <div className="carousel">
+        <div className="episodes">{episodes}</div>
+        <div onClick={this.handleLeft}>↜</div>
+        <div onClick={this.handleRight}>↝</div>
+      </div>
+    );
+  }
+});
+
+var TIP = React.createClass({
+  getInitialState: function () {
+     return {
+       hasPlaybackStarted: false
+     };
+  },
+  startPlayback: function () {
+    this.state.hasPlaybackStarted = true;
+  },
+  render: function () {
+    var preplayClass = this.state.hasPlaybackStarted ? "is-hidden" : "is-visible";
+    var episodes = this.props.data.episodes;
+    var imgUrl = "http://ichef.bbci.co.uk/images/ic/608x342/" + this.props.data.pid + ".jpg"
+    console.log(imgUrl);
+    return (
+      <div className="tip">
+        <div className="tip__journey">
+          <Carousel episodes={episodes}/>
+        </div>
+        <div className="tip__preplay" className={preplayClass}>
+          <div className="tip__backdrop"><img src={imgUrl}/></div>
+          <PlayButton onPlay={this.startPlayback}/>
+          <Name title={this.props.data.title} subtitle={this.props.data.subtitle}/>
+        </div>
+      </div>
+    );
+  }
+});
+    
+var data = {
+  pid: 'p02x2byh',
+  title: 'Eastenders',
+  subtitle: '19/4/2015',
+  guidance: 'This programme contains explicit sex scenes',
+  episodes: [
+    {
+      title: 'Some episode',
+      subtitle: 'Some subtitle'
+    },
+    {
+      title: 'Some episode',
+      subtitle: 'Some subtitle'
+    }
+  ]
+};
+
+React.render(
+  (
+    <TIP data={data}/>
+  ),
+  document.getElementById('content')
+);
